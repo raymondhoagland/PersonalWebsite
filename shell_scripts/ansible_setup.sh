90cd7e991d930f97cc1def4407cc37bee1bcf8b3
Raymond <raymond@Derps.local>
2016-11-09 18:41:37 -0500

    add haproxy boilerplate

--- shell_scripts/ansible_setup.sh
+++ shell_scripts/ansible_setup.sh
@@ -1,3 +1,3 @@
 apt-get -y update
-apt-get -y install python-pip python-dev openssl openssh-server libssl-dev curl wget unzip
+apt-get -y install python-pip python-dev openssl openssh-server libssl-dev curl wget unzip tar
 pip install ansible
