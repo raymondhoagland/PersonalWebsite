1e30ed81a4a186d180ce00de444cd9f7a9775a8e
Raymond <raymond@Derps.local>
2016-11-09 21:06:36 -0500

    try to fix the react code

--- web_server/express_server.js
+++ web_server/express_server.js
@@ -1,6 +1,9 @@
 var express = require("express")
 var app = express()
 
+app.set('views', './views')
+app.set('view engine', 'html') 
+
 app.get("/", function (req, res) {
   res.send("Hello World!")
 })
