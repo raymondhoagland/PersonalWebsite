d676ad48738adb87dbf746bc9902a45f9f19ad74
Raymond <raymond@Derps.local>
2016-11-09 21:50:33 -0500

    try to fix the react code

--- web_server/express_server.js
+++ web_server/express_server.js
@@ -1,19 +1,27 @@
 var express = require("express")
+var nunjucks = require("nunjucks")
 var app = express()
 
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
 app.set('views', './views')
-app.set('view engine', 'html') 
+//app.set('view engine', 'nunjucks')
 
 app.get("/", function (req, res) {
-  res.send("Hello World!")
+    res.send("Hello World!")
 })
 
 app.get("/hello", function (req, res) {
-  res.render("hello", function(err, html){
-      res.send(html);
-  });
+    res.render("hello.njk", {foo: "bar"})
+})
+
+app.get("hello_world.js", function (req, res) {
+    res.render("routes/hello_world.js")
 })
 
 app.listen(3000, function () {
-  console.log("Example app listening on port 3000!")
+    console.log("Example app listening on port 3000!")
 })
